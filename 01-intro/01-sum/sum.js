16dec23bb56cccdd125ad1a2892f75aa4316a55f
Konstantin Klimovich <klimovich@playkot.com>
2023-12-12 19:44:42 +0300

    Решение задачи "Сумма двух чисел"

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,8 @@
 function sum(a, b) {
-  /* ваш код */
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new TypeError('sum() expects only numbers.');
+    }
+    return a + b;
 }
 
 module.exports = sum;
