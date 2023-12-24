c80c6f6bb02bee31714139b9da3a45c298c14837
Konstantin Klimovich <klimovich@playkot.com>
2023-12-24 16:48:14 +0300

    Решение задач по стримам

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,12 +2,22 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
+  #passedBytes = 0;
+
   constructor(options) {
     super(options);
+
+    this.limit = options.limit ?? 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this.#passedBytes += chunk.length;
 
+    if (this.#passedBytes > this.limit) {
+      callback(new LimitExceededError());
+    } else {
+      callback(null, chunk);
+    }
   }
 }
 
